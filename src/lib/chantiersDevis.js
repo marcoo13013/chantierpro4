e25a8f8b4bbc6bffa68bf798cf3138040ce18821
marcoo13013 <marcoo13013@gmail.com>
2026-05-01 18:21:41 +0200

    feat: generate postes from devis tranches

--- src/lib/chantiersDevis.js
+++ src/lib/chantiersDevis.js
@@ -1,17 +1,37 @@
 import { DEVIS_DEMO_PAR_CORPS } from "./devisDemo";
 
 function devisToChantier(devis, index) {
+  var postes = [];
+  var pid = 1;
+  (devis.tranches || []).forEach(function(t) {
+    (t.lignes || []).forEach(function(l) {
+      postes.push({
+        id: pid++,
+        lot: t.titre || "",
+        libelle: l.libelle || "",
+        montantHT: l.totalHT || 0,
+        tempsMO: { heures: l.heuresPrevues || 0, nbOuvriers: 1, detail: "" },
+        fournitures: [{
+          designation: l.libelle || "",
+          qte: l.qte || 1,
+          unite: l.unite || "U",
+          prixAchat: l.puHT || l.prixUnitHT || 0,
+          fournisseur: "",
+        }],
+      });
+    });
+  });
   return {
     id: 100 + index,
-   nom: devis.objet || ("Chantier " + ((devis.client && devis.client.nom) || devis.client || "")),
+    nom: devis.objet || ("Chantier " + ((devis.client && devis.client.nom) || devis.client || "")),
     client: (devis.client && devis.client.nom) || devis.client || "",
-adresseClient: (devis.client && devis.client.adresse) || "",
+    adresseClient: (devis.client && devis.client.adresse) || "",
     adresse: devis.adresseChantier || "",
     statut: devis.statut === "accepté" ? "en cours" : "prospect",
     devisHT: devis.totalHT || 0,
     devisTTC: devis.totalTTC || 0,
     devisId: devis.numero || "",
-    postes: [],
+    postes: postes,
     taches: [],
     salaries: [],
     fournitures: [],
@@ -20,5 +40,5 @@ adresseClient: (devis.client && devis.client.adresse) || "",
 }
 
 export const CHANTIERS_DEMO = Object.values(DEVIS_DEMO_PAR_CORPS).map(
-  (d, i) => devisToChantier(d, i)
+  function(d, i) { return devisToChantier(d, i); }
 );
